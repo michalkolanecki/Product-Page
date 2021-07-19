4dbcef9b3369f43572b2720e605c1e6372812ee5
michalkolanecki <kolanek2@wp.pl>
2021-07-20 00:09:36 +0200

    Add media queries for layout

--- checkout.js
+++ checkout.js
@@ -1,5 +1,27 @@
 const form = document.querySelector('.payment-card__details');
+const decrease = document.querySelector('.product-cart__minus');
+const increase = document.querySelector('.product-cart__plus');
+let result = document.querySelector('.product-cart__result');
+let start = 1;
 
 form.addEventListener('submit', e => {
     e.preventDefault();
 });
+
+function plusClick() {
+    start++;
+    result.innerText = `${start}`;
+    if (start >= 1) {
+        decrease.addEventListener('click', minusClick);
+    }
+}
+increase.addEventListener('click', plusClick);
+
+function minusClick() {
+    start--;
+    result.innerText = `${start + 1}`;
+    if (start <= 0) {
+        decrease.removeEventListener('click', minusClick);
+    } 
+}
+decrease.addEventListener('click', minusClick);
